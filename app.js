70d64a1dc16a8202a33c77eeac75da05928a793d
nigarnuraliyeva0226 <nikanurali0226@gmail.com>
2023-08-27 16:10:04 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,152 @@
+let data = prompt("tarix qeyd edin (noqtelerle ayiraraq)");
+let a = data.split (".")[0];
+let b = data.split (".")[1];
+let c = data.split (".")[2];
+console.log ( a + " " + b + " " + c );
+
+switch (a){
+    case 01:
+    console.log ("1")
+    break
+    case 02:
+        console.log ("2")
+     break
+     case 03:
+        console.log ("3")
+     break
+     case 04:
+        console.log ("4")
+     break
+     case 05:
+        console.log ("5")
+     break
+     case 06:
+        console.log ("6")
+     break
+     case 07:
+        console.log ("7")
+     break
+     case 08:
+        console.log ("8")
+     break
+     case 09:
+        console.log ("9")
+     break
+     case 10:
+        console.log ("10")
+     break
+     case 11:
+        console.log ("11")
+     break
+     case 12:
+        console.log ("11")
+     break
+     case 13:
+        console.log ("13")
+     break
+     case 14:
+        console.log ("14")
+     break
+     case 15:
+        console.log ("15")
+     break
+     case 16:
+        console.log ("16")
+     break
+     case 17:
+        console.log ("17")
+     break
+     case 18:
+        console.log ("18")
+     break
+     case 19:
+        console.log ("19")
+     break
+     case 20:
+        console.log ("20")
+     break
+     case 21:
+        console.log ("21")
+     break
+     case 22:
+        console.log ("22")
+     break
+     case 23:
+        console.log ("23")
+     break
+     case 24:
+        console.log ("24")
+     break
+     case 25:
+        console.log ("25")
+     break
+     case 26:
+        console.log ("26")
+     break
+     case 27:
+        console.log ("27")
+     break
+     case 28:
+        console.log ("28")
+     break
+     case 29:
+        console.log ("29")
+     break
+     case 30:
+        console.log ("30")
+     break
+     case 31:
+        console.log ("31")
+     break
+    default:
+    console.log ("yanlis deyer")
+
+        switch (b) {
+        case 01:
+            console.log ("yanvar")
+         break
+         case 02:
+            console.log ("fevral")
+          break
+          case 03:
+            console.log ("mart")
+          break
+          case 04:
+            console.log ("aprel")
+          break
+          case 05:
+            console.log ("may")
+          break
+          case 06:
+            console.log ("iyun")
+          break
+          case 07:
+            console.log ("iyul")
+          break
+          case 08:
+            console.log ("avqust")
+          break
+          case 09:
+            console.log ("sentyabr")
+          break
+          case 10:
+            console.log ("oktyabr")
+          break
+          case 11:
+            console.log ("noyabr")
+          break
+          case 12:
+            console.log ("fevral")
+          break
+          default:
+            console.log ("yanlis deyer")
+
+            switch (c) {
+                case c:
+                    console.log ("c-ci il")
+                    default :
+                    console.log ("yanlis deyer")
+            }
+}
+}
+
